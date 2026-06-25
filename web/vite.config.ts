45379fd0d8238cb8441f3eeef83dc1b6edc407dd
Taenam Kim <tkim356@wisc.edu>
2026-06-25 11:39:12 -0400

    fix tslib resolution for radix-ui deps

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  optimizeDeps: {
+    include: ["tslib"],
+  },
   server: {
     proxy: {
       "/api": {
